a2e0775caff72ec46b838d99b1fcdb354782a491
unknown <rakaa1820@gmail.com>
2025-08-16 00:32:58 +0800

    js scroll click pada navbar

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// navbar scroll click
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
